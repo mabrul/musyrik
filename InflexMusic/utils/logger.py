b960792688786a329243536ebed43e990ea12877
Rewe <140919868+mabrul@users.noreply.github.com>
2024-06-19 10:31:31 +0700

    Update logger.py

--- InflexMusic/utils/logger.py
+++ InflexMusic/utils/logger.py
@@ -8,18 +8,18 @@ from config import LOG_GROUP_ID
 async def play_logs(message, streamtype):
     if await is_on_off(2):
         logger_text = f"""
-<b>{app.mention} 𝖯𝗅𝖺𝗒 𝖫𝗈𝗀</b>
+<b>{app.mention} Rewe Play 𝖫𝗈𝗀</b>
 
-<b>𝖢𝗁𝖺𝗍 𝖨𝖣 :</b> <code>{message.chat.id}</code>
-<b>𝖢𝗁𝖺𝗍 𝖭𝖺𝗆𝖾 :</b> {message.chat.title}
-<b>𝖢𝗁𝖺𝗍 𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 :</b> @{message.chat.username}
+<b>Chat Id :</b> <code>{message.chat.id}</code>
+<b>Chat Name :</b> {message.chat.title}
+<b>Chat Username :</b> @{message.chat.username}
 
-<b>𝖴𝗌𝖾𝗋 𝖨𝖣 :</b> <code>{message.from_user.id}</code>
-<b>𝖭𝖺𝗆𝖾 :</b> {message.from_user.mention}
-<b>𝖴𝗌𝖾𝗋𝗇𝖺𝗆𝖾 :</b> @{message.from_user.username}
+<b>User Id :</b> <code>{message.from_user.id}</code>
+<b>Name :</b> {message.from_user.mention}
+<b>Username :</b> @{message.from_user.username}
 
-<b>𝖰𝗎𝖾𝗋𝗒 :</b> {message.text.split(None, 1)[1]}
-<b>𝖲𝗍𝗋𝖾𝖺𝗆-𝖳𝗒𝗉𝖾 :</b> {streamtype}"""
+<b>Query :</b> {message.text.split(None, 1)[1]}
+<b>Stream-Type :</b> {streamtype}"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
