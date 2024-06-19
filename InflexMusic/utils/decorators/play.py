9358e8eabf1251d7cd921315b91f50bdea5eaa60
Rewe <140919868+mabrul@users.noreply.github.com>
2024-06-19 11:03:19 +0700

    Update play.py

--- InflexMusic/utils/decorators/play.py
+++ InflexMusic/utils/decorators/play.py
@@ -36,7 +36,7 @@ def PlayWrapper(command):
                 [
                     [
                         InlineKeyboardButton(
-                            text="ʜᴏᴡ ᴛᴏ ғɪx ?",
+                            text="how to fix ?",
                             callback_data="AnonymousAdmin",
                         ),
                     ]
@@ -47,7 +47,7 @@ def PlayWrapper(command):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_GROUP}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    text=f"{app.mention} is under maintenance, visit <a href={SUPPORT_GROUP}>support chat</a> lupa knowing the reason.",
                     disable_web_page_preview=True,
                 )
 
